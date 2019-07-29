3ea040a07d3b73e2e9aba545f4d6e05a337e1c73
weiyinlwy <44257785+weiyinlwy@users.noreply.github.com>
2019-07-30 02:47:55 +0800

    Merge origin/Yin into YinLwy

--- Models/FacultyCampusModel.php
+++ Models/FacultyCampusModel.php
@@ -16,7 +16,19 @@ class FacultyCampusModel extends Model {
         $rows = $this->db->select("* FROM campus");
 
         return $rows;
-
+    }
+    
+    function retrievedByID($id) {
+        $rows = $this->db->select("* FROM campus WHERE campusid = :campusid ", [':campusid' => $id]);
+        return $rows;
+    }
+    
+    function update($id, $value, $column) {
+         $data = array(
+            $column => $value
+        );
+        $where = array('campusid' => $id);
+        $this->db->update('campus', $data, $where);
     }
 
 }
