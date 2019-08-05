1f9c4c785f478645ac76fd479040f7d2479727b6
weiyinlwy <44257785+weiyinlwy@users.noreply.github.com>
2019-08-06 01:34:37 +0800

    Merge origin/Yin into YinLwy

--- Controllers/FacultyAddProgrammeController.php
+++ Controllers/FacultyAddProgrammeController.php
@@ -47,19 +47,118 @@ class FacultyAddProgrammeController extends Controller {
     }
 
     function index() {
-        $rowCampus = $this->modelCampus->retrieveAllCampus();
-        $rowCourse = $this->modelCourse->retrieveAllCourse();
-        $rowCurriculum = $this->modelCurr->retrieveAllCurriculum();
-        $rowMinEntry = $this->modelMinEntry->retrieveAllMinEntry();
         $rowFaculties = $this->modelDpFaculty->retrieveAll();
 
-        $this->view->rowCampus = $rowCampus;
-        $this->view->rowCourse = $rowCourse;
-        $this->view->rowCurriculum = $rowCurriculum;
-        $this->view->rowMinEntry = $rowMinEntry;
         $this->view->rowFaculties = $rowFaculties;
 
         $this->view->render('FacultyAddProgrammeView');
+    }
+
+    function nextAdd() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $programmecode = strtoupper($_POST["programmecode"]);
+            $description = $_POST["description"];
+            $duration = $_POST["duration"];
+            $facultyid = $_POST["faculty"];
+
+            $errorMessage = "";
+            $contextProgCode = new Validator(new ValidationProgCode());
+            $errorMessage = $contextProgCode->executeValidatorStrategy($programmecode);
+
+            $contextDesc = new Validator(new ValidationDesc());
+            $errorMessage .= $contextDesc->executeValidatorStrategy($description);
+
+            if (empty($errorMessage)) {
+                $firstCode = $programmecode[0];
+                $levelofstudyid = "";
+                $words = "";
+                $rowLvlOfStudy = $this->modelDpLoS->retrieveAll();
+                switch ($firstCode) {
+                    case "F":
+                        foreach ($rowLvlOfStudy as $lvlOfStudy) {
+                            if (strtoupper($lvlOfStudy->type) == strtoupper("Foundation")) {
+                                $levelofstudyid = $lvlOfStudy->levelofstudyid;
+                                $words = "Fo";
+                            }
+                        }
+                        break;
+                    case "D":
+                        foreach ($rowLvlOfStudy as $lvlOfStudy) {
+                            if (strtoupper($lvlOfStudy->type) == strtoupper("Diploma")) {
+                                $levelofstudyid = $lvlOfStudy->levelofstudyid;
+                                $words = "Di";
+                            }
+                        }
+                        break;
+                    case "R":
+                        foreach ($rowLvlOfStudy as $lvlOfStudy) {
+                            if (strtoupper($lvlOfStudy->type) == strtoupper("Degree")) {
+                                $levelofstudyid = $lvlOfStudy->levelofstudyid;
+                                $words = "De";
+                            }
+                        }
+                        break;
+                    case "M":
+                        foreach ($rowLvlOfStudy as $lvlOfStudy) {
+                            if (strtoupper($lvlOfStudy->type) == strtoupper("Master")) {
+                                $levelofstudyid = $lvlOfStudy->levelofstudyid;
+                                $words = "Ma";
+                            }
+                        }
+                        break;
+                }
+
+                $this->view->tempProgCode = $programmecode;
+                $this->view->tempProgDesc = $description;
+                $this->view->tempProgDur = $duration;
+                $this->view->tempProgLoS = $levelofstudyid;
+                $this->view->tempProgWords = $words;
+                $this->view->tempProgFac = $facultyid;
+                $_SESSION['progCode'] = $programmecode;
+                $_SESSION['progDesc'] = $description;
+                $_SESSION['progDur'] = $duration;
+                $_SESSION['progLoS'] = $levelofstudyid;
+                $_SESSION['progWords'] = $words;
+                $_SESSION['progFac'] = $facultyid;
+
+                $rowCampus = $this->modelCampus->retrieveAllCampus();
+                $rowCourse = $this->modelCourse->retrieveAllCourse();
+                $rowCurriculum = $this->modelCurr->retrieveAllCurriculum();
+                $rowMinEntry = $this->modelMinEntry->retrieveAllMinEntry();
+                $rowFaculties = $this->modelDpFaculty->retrieveAll();
+
+                $this->view->rowCampus = $rowCampus;
+                $this->view->rowCourse = $rowCourse;
+                $this->view->rowCurriculum = $rowCurriculum;
+                $this->view->rowMinEntry = $rowMinEntry;
+                $this->view->rowFaculties = $rowFaculties;
+
+                $this->view->render('FacultyAddProgrammeView2');
+            } else {
+                echo "<script>alert(\"$errorMessage\"); window.location.href=\"http://localhost/MVC/FacultyAddProgrammeController\";</script>";
+            }
+        } elseif (isset($_SESSION['progCode']) && isset($_SESSION['progDesc']) && isset($_SESSION['progDur']) && isset($_SESSION['progLoS']) && isset($_SESSION['progWords']) && isset($_SESSION['progFac'])) {
+            $this->view->tempProgCode = $_SESSION['progCode'];
+            $this->view->tempProgDesc = $_SESSION['progDesc'];
+            $this->view->tempProgDur = $_SESSION['progDur'];
+            $this->view->tempProgLoS = $_SESSION['progLoS'];
+            $this->view->tempProgWords = $_SESSION['progWords'];
+            $this->view->tempProgFac = $_SESSION['progFac'];
+
+            $rowCampus = $this->modelCampus->retrieveAllCampus();
+            $rowCourse = $this->modelCourse->retrieveAllCourse();
+            $rowCurriculum = $this->modelCurr->retrieveAllCurriculum();
+            $rowMinEntry = $this->modelMinEntry->retrieveAllMinEntry();
+            $rowFaculties = $this->modelDpFaculty->retrieveAll();
+
+            $this->view->rowCampus = $rowCampus;
+            $this->view->rowCourse = $rowCourse;
+            $this->view->rowCurriculum = $rowCurriculum;
+            $this->view->rowMinEntry = $rowMinEntry;
+            $this->view->rowFaculties = $rowFaculties;
+
+            $this->view->render('FacultyAddProgrammeView2');
+        }
     }
 
     function insertDatabase() {
@@ -70,7 +169,6 @@ class FacultyAddProgrammeController extends Controller {
             $courseIsset = isset($_POST['CourseChk']);
             $campusIsset = isset($_POST['CampusChk']);
             $minEntryIsset = isset($_POST['MinEntryChk']);
-            $values = $request->$_POST['CourseChk'];
 
             $errorMessage = "";
             $contextProgCode = new Validator(new ValidationProgCode());
@@ -79,9 +177,6 @@ class FacultyAddProgrammeController extends Controller {
             $contextDesc = new Validator(new ValidationDesc());
             $errorMessage .= $contextDesc->executeValidatorStrategy($description);
 
-            $contextIsset1 = new Validator(new ValidationCourseChk());
-            $errorMessage .= $contextIsset1->executeValidatorStrategy($values);
-            
             $contextIsset1 = new Validator(new ValidationCourseChk());
             $errorMessage .= $contextIsset1->executeValidatorStrategy($courseIsset);
 
@@ -92,95 +187,91 @@ class FacultyAddProgrammeController extends Controller {
             $errorMessage .= $contextIsset3->executeValidatorStrategy($minEntryIsset);
 
             if (empty($errorMessage)) {
-                if ($password == $comfirmedpassword) {
-                    $firstCode = $programmecode[0];
-                    $levelofstudyid = "";
-                    $rowLvlOfStudy = $this->modelDpLoS->retrieveAll();
-                    switch ($firstCode) {
-                        case "F":
-                            foreach ($rowLvlOfStudy as $lvlOfStudy) {
-                                if (strtoupper($lvlOfStudy->type) == strtoupper("Foundation")) {
-                                    $levelofstudyid = $lvlOfStudy->levelofstudyid;
-                                }
+                $firstCode = $programmecode[0];
+                $levelofstudyid = "";
+                $rowLvlOfStudy = $this->modelDpLoS->retrieveAll();
+                switch ($firstCode) {
+                    case "F":
+                        foreach ($rowLvlOfStudy as $lvlOfStudy) {
+                            if (strtoupper($lvlOfStudy->type) == strtoupper("Foundation")) {
+                                $levelofstudyid = $lvlOfStudy->levelofstudyid;
                             }
-                            break;
-                        case "D":
-                            foreach ($rowLvlOfStudy as $lvlOfStudy) {
-                                if (strtoupper($lvlOfStudy->type) == strtoupper("Diploma")) {
-                                    $levelofstudyid = $lvlOfStudy->levelofstudyid;
-                                }
+                        }
+                        break;
+                    case "D":
+                        foreach ($rowLvlOfStudy as $lvlOfStudy) {
+                            if (strtoupper($lvlOfStudy->type) == strtoupper("Diploma")) {
+                                $levelofstudyid = $lvlOfStudy->levelofstudyid;
                             }
-                            break;
-                        case "R":
-                            foreach ($rowLvlOfStudy as $lvlOfStudy) {
-                                if (strtoupper($lvlOfStudy->type) == strtoupper("Degree")) {
-                                    $levelofstudyid = $lvlOfStudy->levelofstudyid;
-                                }
+                        }
+                        break;
+                    case "R":
+                        foreach ($rowLvlOfStudy as $lvlOfStudy) {
+                            if (strtoupper($lvlOfStudy->type) == strtoupper("Degree")) {
+                                $levelofstudyid = $lvlOfStudy->levelofstudyid;
                             }
-                            break;
-                        case "M":
-                            foreach ($rowLvlOfStudy as $lvlOfStudy) {
-                                if (strtoupper($lvlOfStudy->type) == strtoupper("Master")) {
-                                    $levelofstudyid = $lvlOfStudy->levelofstudyid;
-                                }
+                        }
+                        break;
+                    case "M":
+                        foreach ($rowLvlOfStudy as $lvlOfStudy) {
+                            if (strtoupper($lvlOfStudy->type) == strtoupper("Master")) {
+                                $levelofstudyid = $lvlOfStudy->levelofstudyid;
                             }
-                            break;
-                    }
-                    $facultyid = $_POST["faculty"];
-
-                    $credithr = 0;
-                    if (isset($_POST['CourseChk'])) {
-                        foreach ($_POST['CourseChk'] as $courseid) {
-                            $rows = $this->modelCourse->retrievedByID($courseid);
-                            foreach ($rows as $row) {
-                                $value = $row->credithour;
-                            }
-                            $credithr = (int) $credithr + (int) $value;
                         }
-                    }
-
-                    $client = new nusoap_client("http://localhost/MVC/SOAPcalcFee/service.php?wsdl");
-                    $fee = $client->call('calcTotalAmount', array("credithr" => $credithr, "duration" => $duration));
-                    $yearly = $client->call('calcYearlyAmount', array("credithr" => $credithr, "duration" => $duration));
-
-                    $this->modelProg->insert($programmecode, $description, $duration, $levelofstudyid, $facultyid, $fee, $yearly);
-                    $rowProgramme = $this->modelProg->retrieveAllProgramme();
-                    $programmeid = count($rowProgramme);
-
-                    if (isset($_POST['CourseChk'])) {
-                        foreach ($_POST['CourseChk'] as $courses) {
-                            $courseid = $courses;
-                            $this->modelStruct->insert($programmeid, $courseid);
-                        }
-                    }
-
-                    if (isset($_POST['CampusChk'])) {
-                        foreach ($_POST['CampusChk'] as $campuses) {
-                            $campusid = $campuses;
-                            $this->modelProgCampus->insert($programmeid, $campusid);
-                        }
-                    }
-
-                    if (isset($_POST['MinEntryChk'])) {
-                        foreach ($_POST['MinEntryChk'] as $minEntries) {
-                            $minentryid = $minEntries;
-                            $this->modelProgMinEntry->insert($programmeid, $minentryid);
-                        }
-                    }
-
-                    if (isset($_POST['CurriculumChk'])) {
-                        foreach ($_POST['CurriculumChk'] as $curriculum) {
-                            $curriculumid = $curriculum;
-                            $this->modelProgCurr->insert($programmeid, $curriculumid);
-                        }
-                    }
-
-                    echo "<script>alert(\"Successfully Added\"); window.location.href=\"http://localhost/MVC/FacultyAddProgrammeController\";</script>";
-                } else {
-                    echo "<script>alert(\"Unsuccessfully Add!\"); window.location.href=\"http://localhost/MVC/FacultyAddProgrammeController\";</script>";
+                        break;
                 }
+                $facultyid = $_POST["faculty"];
+
+                $credithr = 0;
+                if (isset($_POST['CourseChk'])) {
+                    foreach ($_POST['CourseChk'] as $courseid) {
+                        $rows = $this->modelCourse->retrievedByID($courseid);
+                        foreach ($rows as $row) {
+                            $value = $row->credithour;
+                        }
+                        $credithr = (int) $credithr + (int) $value;
+                    }
+                }
+
+                $client = new nusoap_client("http://localhost/MVC/SOAPcalcFee/service.php?wsdl");
+                $fee = $client->call('calcTotalAmount', array("credithr" => $credithr, "duration" => $duration));
+                $yearly = $client->call('calcYearlyAmount', array("credithr" => $credithr, "duration" => $duration));
+
+                $this->modelProg->insert($programmecode, $description, $duration, $levelofstudyid, $facultyid, $fee, $yearly);
+                $rowProgramme = $this->modelProg->retrieveAllProgramme();
+                $programmeid = count($rowProgramme);
+
+                if (isset($_POST['CourseChk'])) {
+                    foreach ($_POST['CourseChk'] as $courses) {
+                        $courseid = $courses;
+                        $this->modelStruct->insert($programmeid, $courseid);
+                    }
+                }
+
+                if (isset($_POST['CampusChk'])) {
+                    foreach ($_POST['CampusChk'] as $campuses) {
+                        $campusid = $campuses;
+                        $this->modelProgCampus->insert($programmeid, $campusid);
+                    }
+                }
+
+                if (isset($_POST['MinEntryChk'])) {
+                    foreach ($_POST['MinEntryChk'] as $minEntries) {
+                        $minentryid = $minEntries;
+                        $this->modelProgMinEntry->insert($programmeid, $minentryid);
+                    }
+                }
+
+                if (isset($_POST['CurriculumChk'])) {
+                    foreach ($_POST['CurriculumChk'] as $curriculum) {
+                        $curriculumid = $curriculum;
+                        $this->modelProgCurr->insert($programmeid, $curriculumid);
+                    }
+                }
+
+                echo "<script>alert(\"Successfully Added\"); window.location.href=\"http://localhost/MVC/FacultyAddProgrammeController\";</script>";
             } else {
-                echo "<script>alert(\"$errorMessage\"); window.location.href=\"http://localhost/MVC/FacultyAddProgrammeController\";</script>";
+                echo "<script>alert(\"$errorMessage\"); window.location.href=\"http://localhost/MVC/FacultyAddProgrammeController/nextAdd\";</script>";
             }
         }
     }
---
+++ Views/FacultyAddProgrammeView2.php
@@ -0,0 +1,132 @@
+<title>Add Programme</title>
+<div class="container h-100" style="text-align:center">
+    <br/>
+    <h2><strong>Add Programme</strong></h2>
+    <br/>
+    <form action="http://localhost/MVC/FacultyAddProgrammeController/insertDatabase" method="post">
+        <table border="1" class="form" align="center" cellpadding="7" cellspacing="7">
+            <tr>
+                <th><label for="programmecode"><strong>Programme Code: </strong></label></th>
+                <td>
+                    <input name="programmecode" type="text" class="form-control" placeholder="e.g. DIT, RSD, FA1..." value="<?php echo $this->tempProgCode; ?>" required readonly="reasonly" />
+                </td>
+            </tr>
+            <tr>
+                <th><label for="description"><strong>Programme Description: </strong></label></th>
+                <td><input name="description" type="text" class="form-control" placeholder="Enter Programme Description" value="<?php echo $this->tempProgDesc; ?>" required readonly="reasonly" /></td>
+            </tr>
+            <tr>
+                <th><label for="faculty"><strong>Faculty: </strong></label></th>
+                <td>
+                    <select name="faculty" class="form-control" required="" readonly="">
+                        <?php
+                        foreach ($this->rowFaculties as $faculties) {
+                            if ($faculties->facultyid == $this->tempProgFac) {
+                                ?>
+                                <option value="<?php echo $faculties->facultyid ?>" selected="selected"><?php echo $faculties->facultyname ?></option>
+                                <?php
+                            } else {
+                                ?>
+                                <option value="<?php echo $faculties->facultyid ?>"><?php echo $faculties->facultyname ?></option>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <th><label for="duration"><strong>Duration of Study: </strong></label></th>
+                <td>
+                    <select name="duration" class="form-control" required="" readonly="">
+                        <option <?php
+                        if ($this->tempProgDur == "1") {
+                            echo "selected=\"selected\"";
+                        }
+                        ?>>1</option>
+                        <option <?php
+                        if ($this->tempProgDur == "2") {
+                            echo "selected=\"selected\"";
+                        }
+                        ?>>2</option>
+                        <option <?php
+                            if ($this->tempProgDur == "3") {
+                                echo "selected=\"selected\"";
+                            }
+                            ?>>3</option>
+                        <option <?php
+                            if ($this->tempProgDur == "4") {
+                                echo "selected=\"selected\"";
+                            }
+                            ?>>4</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <th><label for="prog"><strong>Programme Course(s): </strong></label></th>
+                <td align="left">
+                    <?php
+                    foreach ($this->rowCourse as $courses) {
+                        if ($courses->status == "active") {
+                            ?>
+                            <input type="checkbox" name="CourseChk[]" class="CourseGrouped" id="<?php echo $courses->courseid ?>" value="<?php echo $courses->courseid ?>"> <?php echo $courses->coursecode . " " . $courses->coursename ?> 
+                            <br/>
+                            <?php
+                        }
+                    }
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <th><label for="campus"><strong>Campuses Offered: </strong></label></th>
+                <td align="left">
+                    <?php
+                    foreach ($this->rowCampus as $campuses) {
+                        if ($campuses->status == "active") {
+                            ?>
+                            <input type="checkbox" name="CampusChk[]" class="CampusGrouped" id="<?php echo $campuses->campusid ?>" value="<?php echo $campuses->campusid ?>"> <?php echo $campuses->campusname ?>
+                            <br/>
+                            <?php
+                        }
+                    }
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <th><label for="minentry"><strong>Minimum Requirement Entries: </strong></label></th>
+                <td align="left">
+                    <?php
+                    foreach ($this->rowMinEntry as $minEntries) {
+                        if ($minEntries->status == "active" && (substr($minEntries->minentryname,0,2) == $this->tempProgWords)) {
+                            ?>
+                            <input type="checkbox" name="MinEntryChk[]" class="MinEntryGrouped" id="<?php echo $minEntries->minentryid ?>" value="<?php echo $minEntries->minentryid ?>"> <?php echo $minEntries->minentryname ?>
+                            <br/>
+                            <?php
+                        }
+                    }
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <th><label for="curriculum"><strong>Incorporation of Professional Curriculum: </strong></label></th>
+                <td align="left">
+                    <?php
+                    foreach ($this->rowCurriculum as $curriculum) {
+                        if ($curriculum->status == "active") {
+                            ?>
+                            <input type="checkbox" name="CurriculumChk[]" class="CurriculumGrouped" id="<?php echo $curriculum->curriculumid ?>" value="<?php echo $curriculum->curriculumid ?>"> <?php echo $curriculum->curriculumname ?>
+                            <br/>
+        <?php
+    }
+}
+?>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td class="submit-button-right">
+                    <input class="btn btn-info" type="submit" value="Submit" title="Submit" />                      
+            </tr>
+        </table>
+    </form>
+</div>
